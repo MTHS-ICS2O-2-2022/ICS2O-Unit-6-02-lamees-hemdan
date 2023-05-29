ac0266becd0ed5c61e718a8fda68de05fc203e81
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-29 01:40:10 +0000

    did 6-02

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Lamees Hemdan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  // this program will keep score of how many times you clicked on the cookie
+
+  // process
+  if (localStorage.clicks) {
+    localStorage.clicks = Number(localStorage.clicks) + 1
+  } else {
+    localStorage.clicks = 1
+  }
+
+// output
+  document.getElementById("clicks").innerHTML = "You have clicked the cookie " + localStorage.clicks + " times"
+}
